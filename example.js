4d5271e4375c7cd88a2f673cc5306645196c762c
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-08-10 19:12:41 -0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,11 @@
+UpdateCustomRPC({
+    name: "ffffff",
+    type: 0,
+    appId: 1,
+    buttons: [
+        {
+            text: 'aaaaa',
+            url: 'https://example.com/'
+        }
+    ],
+});
